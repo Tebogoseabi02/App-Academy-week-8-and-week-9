3893e911a0fcfef1b4e5dd072f007f0be24da876
Tebogoseabi02 <t.tebogoseabi@icloud.com>
2025-07-04 14:48:50 -0700

    The work I have done for Week 8 and 9

---
+++ python.py
@@ -0,0 +1,30 @@
+#Operators
+
+# Addition (+)
+# Subtraction (-)
+# Multiplication (*)
+# Division (/)
+# Modulus (%)
+# Exponent (**)
+
+'''
+x = 10
+y = 2
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(5%2)
+print(x**y)
+
+'''
+x = 10
+x -= 2
+
+print(x)
+
+
+
+
